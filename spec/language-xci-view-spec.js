5aa64c601302d41c5e7b4bc73d45b1175fabbc8a
Matt Heffernan <slithymatt@hotmail.com>
2020-07-29 00:54:09 -0400

    Defined keywords, comments

--- spec/language-xci-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import LanguageXciView from '../lib/language-xci-view';
-
-describe('LanguageXciView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
